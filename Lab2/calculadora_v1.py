6e12c310c9a64c0958b9641d27db93b6e8b59c48
matheus tatibano <matheus.tatibano@gmail.com>
2023-03-06 12:06:53 -0300

    teste

---
+++ Lab2/calculadora_v1.py
@@ -0,0 +1,19 @@
+# Calculadora em Python
+
+# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos até aqui no curso. 
+# A solução será apresentada no próximo capítulo!
+
+print("\n******************* Calculadora em Python *******************")
+
+
+print("\nSelecione o número da operacao desejada"
+    
+
+    "\n1 - Soma"
+    "\n2 - Subtração"
+    "\n3 - Multiplicação"
+    "\n4 - Divisão")
+
+        int(input("Digite sua opçao(1/2/3/4):"))
+
+
